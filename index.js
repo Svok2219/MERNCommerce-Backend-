5c6542a41c20b23d73997d340276567430800e82
Svok2219 <shuvokoiri0@gmail.com>
2022-10-23 18:00:55 +0600

    longwaytogo

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ mongoose.connect(
     console.log(err);
   });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT ;
 app.listen(port, () => console.log(`Server running on port ${port}`));
